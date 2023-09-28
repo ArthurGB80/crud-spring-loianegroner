a088f541a1ae8900302f2169e7a9101d73fc5b8d
arthurgb80 <arthurgularte80@gmail.com>
2023-09-28 15:53:40 +0300

    Terminando a aula 21 e corrigindo caminho do botao "novo curso" (new)

--- src/main/java/com/arthurgb80/crudspring/controller/CourseController.java
+++ src/main/java/com/arthurgb80/crudspring/controller/CourseController.java
@@ -3,11 +3,14 @@ package com.arthurgb80.crudspring.controller;
 import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.arthurgb80.crudspring.model.Course;
@@ -31,23 +34,10 @@ public class CourseController {
 
     // @RequestMapping(method = RequestMethod.POST)
     @PostMapping
-    public void create(@RequestBody Course course) {
-        // System.out.println(course.getName());
-        courseRepository.save(course);
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        return courseRepository.save(course);
+        // return ResponseEntity.status(HttpStatus.CREATED)
+        //         .body(courseRepository.save(course));
     }
-
-    /*
-     * @Bean
-     * CommandLineRunner intDatabase(CourseRepository courseRepository) {
-     * return args -> {
-     * courseRepository.deleteAll();
-     * 
-     * Course c = new Course();
-     * c.setName("Angular com Spring");
-     * c.setCategory("Front-end");
-     * 
-     * courseRepository.save(c);
-     * };
-     * }
-     */
 }
