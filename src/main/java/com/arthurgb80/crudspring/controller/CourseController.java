5463be26c60884ff7f3b43c9793fc0d2ae32c6fa
arthurgb80 <arthurgularte80@gmail.com>
2023-09-29 17:02:40 +0300

    Aulas 22 a 24 - modificações apenas no angular

--- src/main/java/com/arthurgb80/crudspring/controller/CourseController.java
+++ src/main/java/com/arthurgb80/crudspring/controller/CourseController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
