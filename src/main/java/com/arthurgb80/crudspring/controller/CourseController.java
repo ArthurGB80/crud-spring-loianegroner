40904eff25ae93bb4c579a4450c65cd0adf28de1
arthurgb80 <arthurgularte80@gmail.com>
2023-10-04 16:06:30 +0300

    Aulas 31 e 32 e revisao

--- src/main/java/com/arthurgb80/crudspring/controller/CourseController.java
+++ src/main/java/com/arthurgb80/crudspring/controller/CourseController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -50,7 +52,7 @@ public class CourseController {
         // .body(courseRepository.save(course));
     }
 
-    @PostMapping("/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<Course> update(@PathVariable Long id,
             @RequestBody Course course) {
         return courseRepository.findById(id)
@@ -59,6 +61,17 @@ public class CourseController {
                     recordFound.setCategory(course.getCategory());
                     Course updated = courseRepository.save(recordFound);
                     return ResponseEntity.ok().body(updated);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        courseRepository.deleteById(id);
+        return courseRepository.findById(id)
+                .map(recordFound -> {
+                    courseRepository.deleteById(id);
+                    return ResponseEntity.noContent().<Void>build();
                 })
                 .orElse(ResponseEntity.notFound().build());
     }
